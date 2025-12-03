bb9be68d3535c4046488c8f3ec683590f07ebdfc
Panagopoulos Ioannis <johnpan738@gmail.com>
2025-12-03 20:35:04 +0200

    feat: add new class for addition

--- src/gr/aueb/cf/testgit/AddApp.java
+++ src/gr/aueb/cf/testgit/AddApp.java
@@ -1,4 +1,14 @@
 package gr.aueb.cf.testgit;
 
 public class AddApp {
+
+
+    public void main(String[] args) {
+        System.out.println( add( 1, 2));
+
+    }
+
+    public static int add(int a, int b){
+        return a + b;
+    }
 }
